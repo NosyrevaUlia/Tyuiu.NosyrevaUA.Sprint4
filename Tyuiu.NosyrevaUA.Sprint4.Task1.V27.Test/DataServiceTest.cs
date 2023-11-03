147266f50040b26a22c93a9b2bad833916f5bce3
Tyuiu.NosyrevaUA@outlook.com <ulianosyreva752@gmail.com>
2023-11-04 00:13:40 +0500

    Task1

---
+++ Tyuiu.NosyrevaUA.Sprint4.Task1.V27.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.NosyrevaUA.Sprint4.Task1.V27.Lib;
+
+namespace Tyuiu.NosyrevaUA.Sprint4.Task1.V27.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[] numsArray = { 8, 1, 4, 4, 1, 2, 3, 5, 1, 5 };
+            int res = ds.Calculate(numsArray);
+            Assert.AreEqual(res, 256);
+        }
+    }
+}
